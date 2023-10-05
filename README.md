8cd1fbf35b943aff667e2a4b29568b16cb478add
gev.org <thecarguyarm@gmail.com>
2023-10-05 14:23:59 +0400

    README and mongod  running shell script

---
+++ README.md
@@ -0,0 +1,18 @@
+# CRUD APP With User Authentication
+&ensp;&ensp;&ensp; CRUD application where users can log in or register to add books 
+to their private collection. Each user is able to view his own books. Authentication of
+users are done using JWT, passwords in database are saved in encrypted code.
+## Start The App
+
+- Run the server:
+```shell
+    npm run start
+```
+- Run the MongoDB 
+```shell
+    chmod +x start-mongodb.sh
+```
+```shell
+    ./start-mongodb.sh
+```
+App will run on [local port 3000](http://localhost:3000/).
---
+++ start-mongodb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the name and version of the MongoDB Docker image
+MONGODB_IMAGE="mongo:latest"
+
+# Check if the Docker image exists locally
+if docker image inspect "$MONGODB_IMAGE" &>/dev/null; then
+    echo "MongoDB image already exists."
+else
+    # Pull the MongoDB image from Docker Hub if it doesn't exist locally
+    echo "Pulling MongoDB image..."
+    docker pull "$MONGODB_IMAGE"
+fi
+
+docker run -d -p 27017:27017 --name my-mongodb "$MONGODB_IMAGE"
+
+# Check if the container is running
+if [ "$(docker inspect -f '{{.State.Running}}' my-mongodb 2>/dev/null)" == "true" ]; then
+    echo "MongoDB container is running."
+else
+    echo "Failed to start MongoDB container."
+fi
